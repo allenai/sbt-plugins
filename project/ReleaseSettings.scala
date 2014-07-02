541432048d9ce984902fb60bfd512f566e5c2d76
Mark Schaake <marks@allenai.org>
2014-07-02 14:34:07 -0700

    Remove dead code

--- project/ReleaseSettings.scala
+++ project/ReleaseSettings.scala
@@ -10,7 +10,8 @@ import java.util.Calendar
 // TODO(markschaake): consider turning this into a standalone plugin
 object ReleaseSettings {
 
-  private def calendarVersion(cal: Calendar): String = {
+  private def todayVersion: String = {
+    val cal = Calendar.getInstance()
     def zeroPad(n: Int) =
       if (n < 10) s"0${n}"
       else s"${n}"
@@ -19,14 +20,6 @@ object ReleaseSettings {
     val month = zeroPad(cal.get(Calendar.MONTH) + 1)
     val day = zeroPad(cal.get(Calendar.DAY_OF_MONTH))
     s"${year}.${month}.${day}"
-  }
-
-  private def todayVersion = calendarVersion(Calendar.getInstance())
-
-  private def tomorrowVersion = {
-    val cal = Calendar.getInstance
-    cal.add(Calendar.DATE, 1)
-    calendarVersion(cal)
   }
 
   val VersionPattern = """(\d+\.\d+\.\d+)-(\d+)(?:-SNAPSHOT)?""".r
