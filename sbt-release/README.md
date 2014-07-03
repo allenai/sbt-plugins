f5b738a0845a199b0d52e85691fcd5985f66ec09
Mark Schaake <marks@allenai.org>
2014-07-03 14:07:13 -0700

    AllenAiReleasePlugin

---
+++ sbt-release/README.md
@@ -0,0 +1,3 @@
+# allenai-sbt-release
+
+AutoPlugin that wraps [sbt-release](https://github.com/sbt/sbt-release) providing a custom versioning scheme.
---
+++ sbt-release/src/main/scala/AllenaiReleasePlugin.scala
@@ -0,0 +1,43 @@
+package org.allenai.sbt.release
+
+import sbt._
+import sbt.Keys._
+
+import sbtrelease._
+import sbtrelease.ReleaseStateTransformations._
+import sbtrelease.ReleasePlugin.ReleaseKeys._
+
+import java.text.SimpleDateFormat
+import java.util.Date
+
+object AllenaiReleasePlugin extends AutoPlugin {
+
+  override def requires = plugins.JvmPlugin
+
+  private def todayVersion: String = {
+    val df = new SimpleDateFormat("yyyy.MM.dd")
+    df.format(new Date())
+  }
+
+  val VersionPattern = """(\d+\.\d+\.\d+)-(\d+)(?:-SNAPSHOT)?""".r
+
+  def incrementVersion(prev: String): String = {
+    prev match {
+      case VersionPattern(prefix, num) => s"${prefix}-${num.toInt + 1}"
+      case _ => throw new IllegalStateException(s"Invalid version number: ${prev}")
+    }
+  }
+
+  override lazy val projectSettings = ReleasePlugin.releaseSettings ++ Seq(
+    releaseVersion := { ver =>
+      val today = todayVersion
+      if (ver.startsWith(today)) {
+        ver.replace("-SNAPSHOT", "")
+      } else {
+        s"${today}-0"
+      }
+    },
+    nextVersion := { ver =>
+      s"${incrementVersion(ver)}-SNAPSHOT"
+    })
+}
