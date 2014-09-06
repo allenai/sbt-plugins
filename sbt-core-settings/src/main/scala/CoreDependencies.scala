35ce8c69440aca00ea761d3ddc37c1859396c9ad
Mark Schaake <marks@allenai.org>
2014-09-06 06:13:31 -0700

    Cleanup logging dependencies

--- sbt-core-settings/src/main/scala/CoreDependencies.scala
+++ sbt-core-settings/src/main/scala/CoreDependencies.scala
@@ -8,28 +8,34 @@ import sbt.Keys._
   */
 object CoreDependencies {
   val defaultScalaVersion = "2.10.4"
-  val slf4jVersion = "1.7.7"
-  // The logging API to use. This should be the only logging dependency of any API artifact
-  // (anything that's going to be depended on outside of this SBT project).
-  val slf4jApi = "org.slf4j" % "slf4j-api" % slf4jVersion
-  // Removes all unneeded log4j & slf4j implementations from the given modules. Adds in a single
+
+  object Logging {
+    val slf4jVersion = "1.7.7"
+    // The logging API to use. This should be the only logging dependency of any API artifact
+    // (anything that's going to be depended on outside of this SBT project).
+    val slf4jApi = "org.slf4j" % "slf4j-api" % slf4jVersion
+    val logbackCore = "ch.qos.logback" % "logback-core" % "1.1.2"
+    val logbackClassic = "ch.qos.logback" % "logback-classic" % "1.1.2"
+  }
+
   // slf4j implementation (logback), and the log4j -> slf4j bridge.
   // This should be called on libraryDependencies like:
   // addLoggingDependencies(libraryDependencies)
   def addLoggingDependencies(deps: SettingKey[Seq[ModuleID]]): Seq[Setting[Seq[ModuleID]]] = {
+    import Logging._
     val cleanedDeps = deps ~= { seq =>
       seq map { module =>
         // Exclude the transitive dependencies that might mess things up for us.
         // slf4j replaces log4j.
         (module exclude("log4j", "log4j")
-           // We're using logback as the slf4j implementation, and we're providing it below.
-           exclude("org.slf4j", "slf4j-log4j12")
-           exclude("org.slf4j", "slf4j-jdk14")
-           exclude("org.slf4j", "slf4j-jcl")
-           exclude("org.slf4j", "slf4j-simple")
-           // We'll explicitly provide the logback version; this avoids having to do an override.
-           exclude("ch.qos.logback", "logback-core")
-           exclude("ch.qos.logback", "logback-classic"))
+          // We're using logback as the slf4j implementation, and we're providing it below.
+          exclude("org.slf4j", "slf4j-log4j12")
+          exclude("org.slf4j", "slf4j-jdk14")
+          exclude("org.slf4j", "slf4j-jcl")
+          exclude("org.slf4j", "slf4j-simple")
+          // We'll explicitly provide the logback version; this avoids having to do an override.
+          exclude("ch.qos.logback", "logback-core")
+          exclude("ch.qos.logback", "logback-classic"))
       }
     }
     // Now, add the logging libraries.
@@ -38,8 +44,8 @@ object CoreDependencies {
       // Bridge log4j logging to slf4j.
       "org.slf4j" % "log4j-over-slf4j" % slf4jVersion,
       // Use logback for the implementation.
-      "ch.qos.logback" % "logback-core" % "1.1.2",
-      "ch.qos.logback" % "logback-classic" % "1.1.2")
+      logbackCore,
+      logbackClassic)
     Seq(cleanedDeps, logbackDeps)
   }
 
@@ -51,6 +57,7 @@ object CoreDependencies {
   def akkaModule(id: String) = "com.typesafe.akka" %% s"akka-$id" % akkaVersion
   val akkaActor = akkaModule("actor")
   val akkaLogging = akkaModule("slf4j")
+  val akkaTestkit = akkaModule("testkit")
 
   // Spray
   val sprayVersion = "1.3.1"
@@ -65,8 +72,11 @@ object CoreDependencies {
   val sprayJson = "io.spray" %%  "spray-json" % "1.2.6"
 
   val defaultDependencyOverrides = Set(
-    slf4jApi,
-    "org.scala-lang" % "scala-library" % defaultScalaVersion
+    "org.scala-lang" % "scala-library" % defaultScalaVersion,
+    typesafeConfig,
+    Logging.slf4jApi,
+    Logging.logbackCore,
+    Logging.logbackClassic
   )
 
 }
