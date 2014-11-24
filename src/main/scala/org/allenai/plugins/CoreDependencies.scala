e2dabc66a59f951224ad73e9fdd988f664b184b3
Michael Schmitz <michael@schmitztech.com>
2014-11-24 09:16:02 -0800

    Revert to last release of common.

--- src/main/scala/org/allenai/plugins/CoreDependencies.scala
+++ src/main/scala/org/allenai/plugins/CoreDependencies.scala
@@ -52,7 +52,7 @@ trait CoreDependencies {
   }
 
   // AI2 common libraries
-  private def common(name: String) = "org.allenai.common" %% s"common-$name" % "2014.11.24-0"
+  private def common(name: String) = "org.allenai.common" %% s"common-$name" % "2014.11.05-0"
   val allenAiCommon = common("core")
   val allenAiTestkit = common("testkit")
   val allenAiWebapp = common("webapp")
