95856c742d4a39358e7d3d187c3d7f68024af639
Michael Schmitz <michael@schmitztech.com>
2015-01-02 13:43:19 -0800

    Remove later validation.

--- src/main/scala/org/allenai/plugins/DeployPlugin.scala
+++ src/main/scala/org/allenai/plugins/DeployPlugin.scala
@@ -219,9 +219,7 @@ object DeployPlugin extends AutoPlugin {
     // operations.
     val sshCommand = {
       val pemEnvVar = "AWS_PEM_FILE"
-      val sshKeyfile = sys.env.get(pemEnvVar).getOrElse {
-        throw new IllegalArgumentException(s"Environment variable ${pemEnvVar} undefined.")
-      }
+      val sshKeyfile = sys.env.get(pemEnvVar).get
       val sshUser = configMap("deploy.user.ssh_username")
       Seq("ssh", "-i", sshKeyfile, "-l", sshUser)
     }
