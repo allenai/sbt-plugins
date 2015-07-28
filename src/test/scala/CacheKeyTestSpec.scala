52e788d17e59335d3bbee1bcd7ba01c1b8d96efe
Ryan Benmalek <ryanai3@hotmail.com>
2015-07-28 10:36:32 -0700

    moved tests to UnitSpec style

--- src/test/scala/CacheKeyTestSpec.scala
+++ src/test/scala/CacheKeyTestSpec.scala
@@ -1,22 +1,20 @@
-import org.scalatest._
-
+import org.allenai.common.testkit.UnitSpec
 import scala.sys.process._
-class CacheKeyTest {
-  private var cacheKey1 = Option("cacheKey1")
-  private var cacheKey2 = Option("cacheKey2")
+import scala.io.Source
+
+class CacheKeyTestSpec extends UnitSpec {
+  private var cacheKey1: Option[String] = None
+  private var cacheKey2: Option[String] = None
   private var gitCommitsMade = 0
   private var dependenciesAdded = 0
 
+  // Helper methods
   def cleanUp(): Unit = {
     // Remove dependency that was added (sed remove last line from file)
     (1 to gitCommitsMade) foreach (x => Seq("sed", "-i", "$ d", "build.sbt").!!)
     // Remove git commit that was added (git remove last commit & reset file state)
     (1 to dependenciesAdded) foreach (x => Seq("git", "reset", "--hard", "HEAD~1").!!)
   }
-
-  def generateCacheKey1() = { cacheKey1 = generateCacheKey() }
-
-  def generateCacheKey2() = { cacheKey2 = generateCacheKey() }
 
   def generateCacheKey(): Option[String] = {
     runStageAndCacheKey()
@@ -39,76 +37,42 @@ class CacheKeyTest {
   }
 
   def getCacheKey(): Option[String] = {
-    import scala.io.Source
     try {
       Some(Source.fromFile("./test-projects/test-deploy/service/target/universal/stage/conf/cacheKey.Sha1").getLines.mkString)
     } catch { case _ => None }
   }
 
   def areCacheKeysEqual() = cacheKey1 == cacheKey2
-}
 
-class CacheKeyTestSpec extends FeatureSpec with GivenWhenThen {
-  feature("CacheKey Generation") {
-    scenario("CacheKeys are injected properly") {
-      Given("we have generated a cacheKey")
-      val cacheKeyTest = new CacheKeyTest()
-      cacheKeyTest.generateCacheKey1()
+  "A cachekey" should "be injected properly" in {
+    val cacheKey = generateCacheKey()
+    assert(cacheKey != None)
+  }
 
-      Then("the cachekey should exist in the right location")
-      assert(cacheKeyTest.getCacheKey() != None)
-    }
+  "A cachekey" should "be the same on rebuild" in {
+    val cacheKey1 = generateCacheKey()
+    val cacheKey2 = generateCacheKey()
+    assert(cacheKey1 == cacheKey2)
+  }
 
-    scenario("CacheKeys remain the same on rebuild") {
-      Given("we have generated a cacheKey")
-      val cacheKeyTest = new CacheKeyTest()
-      cacheKeyTest.generateCacheKey1()
+  "A cachekey" should "change on dependency changes" in {
+    val cacheKey1 = generateCacheKey()
+    addADependency()
+    val cacheKey2 = generateCacheKey()
+    assert(cacheKey1 != cacheKey2)
+  }
 
-      When("we generate a cacheKey again")
-      cacheKeyTest.generateCacheKey2()
+  "A cachekey" should "change on git commits to local dependencies" in {
+    val cacheKey1 = generateCacheKey()
+    makeGitCommit()
+    val cacheKey2 = generateCacheKey()
+    assert(cacheKey1 != cacheKey2)
+  }
 
-      Then("the cachekeys should be the same")
-      assert(cacheKeyTest.areCacheKeysEqual())
-    }
-
-    scenario("CacheKeys change on dependency changes") {
-      Given("we have generated a cacheKey")
-      val cacheKeyTest = new CacheKeyTest()
-      cacheKeyTest.generateCacheKey1()
-
-      When("we change the dependencies and generate a cacheKey again")
-      cacheKeyTest.addADependency()
-      cacheKeyTest.generateCacheKey2()
-
-      Then("the cachekeys should be different")
-      assert(!cacheKeyTest.areCacheKeysEqual())
-    }
-
-    scenario("CacheKeys change on git commits to local dependencies") {
-      Given("we have generated a cacheKey")
-      val cacheKeyTest = new CacheKeyTest()
-      cacheKeyTest.generateCacheKey1()
-
-      When("we make a git commit to a local dependency and generate a cacheKey again")
-      cacheKeyTest.makeGitCommit()
-      cacheKeyTest.generateCacheKey2()
-
-      Then("the cacheKeys should be different")
-      assert(!cacheKeyTest.areCacheKeysEqual())
-    }
-
-    scenario("CacheKeys change on git commits to src dir of project") {
-      Given("we have generated a cacheKey")
-      val cacheKeyTest = new CacheKeyTest()
-      cacheKeyTest.generateCacheKey1()
-
-      When("we make a git commit to src dir and generate a cacheKey again")
-      cacheKeyTest.makeGitCommit()
-      cacheKeyTest.generateCacheKey2()
-
-      Then("the cacheKeys should be different")
-      assert(!cacheKeyTest.areCacheKeysEqual())
-    }
-
+  "A cachekey" should "change on git commits to src dir of project"{
+    val cacheKey1 = generateCacheKey()
+    makeGitCommit()
+    val cacheKey2 = generateCacheKey()
+    assert(cacheKey1 != cacheKey2)
   }
 }
